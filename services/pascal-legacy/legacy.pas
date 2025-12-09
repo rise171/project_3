0da8c8e3813135b70a27517abc0fc36ba3282a1a
rise171 <krispetrova206@gmail.com>
2025-12-09 13:30:18 +0300

    update legacy

--- services/pascal-legacy/legacy.pas
+++ services/pascal-legacy/legacy.pas
@@ -1,72 +1,84 @@
 program LegacyCSV;
-
 {$mode objfpc}{$H+}
 
 uses
-  SysUtils, DateUtils, Process;
+  SysUtils, DateUtils, Process, BaseUnix,
+  Config, CSVGen;
 
-function GetEnvDef(const name, def: string): string;
-var v: string;
+function EnsureDir(const p: string): Boolean;
 begin
-  v := GetEnvironmentVariable(name);
-  if v = '' then Exit(def) else Exit(v);
+  if not DirectoryExists(p) then
+    Result := CreateDir(p)
+  else
+    Result := True;
 end;
 
-function RandFloat(minV, maxV: Double): Double;
+function ImportToPg(const fullpath: string): integer;
+var pghost, pgport, pguser, pgpass, pgdb: string;
+    proc: TProcess;
 begin
-  Result := minV + Random * (maxV - minV);
-end;
-
-procedure GenerateAndCopy();
-var
-  outDir, fn, fullpath, pghost, pgport, pguser, pgpass, pgdb, copyCmd: string;
-  f: TextFile;
-  ts: string;
-begin
-  outDir := GetEnvDef('CSV_OUT_DIR', '/data/csv');
-  ts := FormatDateTime('yyyymmdd_hhnnss', Now);
-  fn := 'telemetry_' + ts + '.csv';
-  fullpath := IncludeTrailingPathDelimiter(outDir) + fn;
-
-  // write CSV
-  AssignFile(f, fullpath);
-  Rewrite(f);
-  Writeln(f, 'recorded_at,voltage,temp,source_file');
-  Writeln(f, FormatDateTime('yyyy-mm-dd hh:nn:ss', Now) + ',' +
-             FormatFloat('0.00', RandFloat(3.2, 12.6)) + ',' +
-             FormatFloat('0.00', RandFloat(-50.0, 80.0)) + ',' +
-             fn);
-  CloseFile(f);
-
-  // COPY into Postgres
   pghost := GetEnvDef('PGHOST', 'db');
   pgport := GetEnvDef('PGPORT', '5432');
   pguser := GetEnvDef('PGUSER', 'monouser');
   pgpass := GetEnvDef('PGPASSWORD', 'monopass');
   pgdb   := GetEnvDef('PGDATABASE', 'monolith');
 
-  // Use psql with COPY FROM PROGRAM for simplicity
-  // Here we call psql reading from file
-  copyCmd := 'psql "host=' + pghost + ' port=' + pgport + ' user=' + pguser + ' dbname=' + pgdb + '" ' +
-             '-c "\copy telemetry_legacy(recorded_at, voltage, temp, source_file) FROM ''' + fullpath + ''' WITH (FORMAT csv, HEADER true)"';
-  // Mask password via env
   SetEnvironmentVariable('PGPASSWORD', pgpass);
-  // Execute
-  fpSystem(copyCmd);
+
+  proc := TProcess.Create(nil);
+  try
+    proc.Executable := 'psql';
+    proc.Parameters.Add(Format(
+      'host=%s port=%s user=%s dbname=%s',
+      [pghost, pgport, pguser, pgdb]
+    ));
+    proc.Parameters.Add('-c');
+    proc.Parameters.Add(
+      '\copy telemetry_legacy(recorded_at,voltage,temp,source_file)' +
+      ' FROM STDIN WITH (FORMAT csv, HEADER true)'
+    );
+    proc.Options := [poUsePipes];
+    proc.Execute;
+
+    with proc.Input do begin
+      LoadFromFile(fullpath);
+      Close;
+    end;
+
+    proc.WaitOnExit;
+    Result := proc.ExitStatus;
+  finally
+    proc.Free;
+  end;
 end;
 
-var period: Integer;
+var
+  outDir, fn, fullpath: string;
+  ts: string;
+  period: Integer;
 begin
   Randomize;
   period := StrToIntDef(GetEnvDef('GEN_PERIOD_SEC', '300'), 300);
+
+  outDir := GetEnvDef('CSV_OUT_DIR', '/data/csv');
+  if not EnsureDir(outDir) then
+    raise Exception.Create('Cannot access CSV_OUT_DIR: ' + outDir);
+
+  WriteLn('[legacy] started');
+
   while True do
   begin
-    try
-      GenerateAndCopy();
-    except
-      on E: Exception do
-        WriteLn('Legacy error: ', E.Message);
-    end;
-    Sleep(period * 1000);
+    ts := FormatDateTime('yyyymmdd_hhnnss', Now);
+    fn := 'telemetry_' + ts + '.csv';
+    fullpath := IncludeTrailingPathDelimiter(outDir) + fn;
+
+    CSVGen.GenerateCSVFile(fullpath);
+
+    if ImportToPg(fullpath) <> 0 then
+      WriteLn('[legacy] import failed: ', fullpath)
+    else
+      WriteLn('[legacy] imported: ', fullpath);
+
+    fpSelect(0, nil, nil, nil, period * 1000);
   end;
 end.
