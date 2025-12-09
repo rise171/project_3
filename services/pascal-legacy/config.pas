b7ced99dd3ab10ca894150b2890dd14391712007
rise171 <krispetrova206@gmail.com>
2025-12-09 10:36:42 +0300

    add pascal config

---
+++ services/pascal-legacy/config.pas
@@ -0,0 +1,19 @@
+unit Config;
+{$mode objfpc}{$H+}
+
+interface
+
+function GetEnvDef(const name, def: string): string;
+
+implementation
+
+uses SysUtils;
+
+function GetEnvDef(const name, def: string): string;
+var v: string;
+begin
+  v := GetEnvironmentVariable(name);
+  if v = '' then Exit(def) else Exit(v);
+end;
+
+end.
